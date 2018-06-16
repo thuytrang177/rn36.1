5cafd60d430f32216e2a19b5e619eef38266b3a4
Nguyen Thuy Trang <nthuytrang1788@gmail.com>
2018-06-16 10:05:23 +0700

    update btvn

--- btvn.js
+++ btvn.js
@@ -1,2 +1,32 @@
-var x = 10;
-var result;
+// bai 1
+
+function tinhTong() {
+    var sum = 0;
+    var n = 7;
+
+    if (n > 0) {
+        for (i = 1; i <= n; i++) {
+            sum += (i*i);
+        }
+        console.log('S = '+ sum);
+        
+    } else {
+        console.log('khong hop le');
+    }
+}
+
+
+tinhTong();
+
+// bai 2 
+function mySum () {
+    var sum = 0; 
+    for (i = 1; i <= 1000; i++) {
+        sum += i;
+        if (sum >= 1000) {
+            console.log('n nho nhat = ' + i);
+            break;
+        }
+    }
+}
+mySum();
