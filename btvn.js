e8e473fd3c123cab58733b6b5d9efa84d003c78f
Nguyen Thuy Trang <nthuytrang1788@gmail.com>
2018-06-18 13:18:22 +0700

    update new

--- btvn.js
+++ btvn.js
@@ -15,18 +15,46 @@ function tinhTong() {
     }
 }
 
-
 tinhTong();
 
 // bai 2 
 function mySum () {
     var sum = 0; 
-    for (i = 1; i <= 1000; i++) {
+    while (i >= 1) {
         sum += i;
         if (sum >= 1000) {
             console.log('n nho nhat = ' + i);
             break;
         }
+        i++;
     }
 }
 mySum();
+
+// bai 3
+
+function isPrime (n) {
+    if (n < 2) {
+        return false;
+    }
+    for (i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+
+}
+
+const tichPrime = (N) => {
+    var tich = 1;
+    for (j = 2; j <= N; j++) {
+        if (isPrime(j) == true); {
+            tich *= j;
+        }
+    }
+    console.log(tich);
+}
+tichPrime(17);
+
+
